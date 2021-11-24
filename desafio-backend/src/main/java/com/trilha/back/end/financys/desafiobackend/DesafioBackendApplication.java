7317f0759d3fe728c344cb83f050553451fb365d
Gabriel Ryuji Barros <gryujiba@everis.com>
2021-11-24 14:50:28 -0300

    adicionando o spring boot no projeto

---
+++ desafio-backend/src/main/java/com/trilha/back/end/financys/desafiobackend/DesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trilha.back.end.financys.desafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendApplication.class, args);
+	}
+
+}
